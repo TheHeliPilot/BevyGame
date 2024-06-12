9d20b0f1d8508bdac983a50e5abece81f529e1cd
PirateOwnerOfTheBoat <michal.fenik@protonmail.com>
2024-06-12 13:45:02 +0200

    Vsync off

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,30 @@ use iyes_perf_ui::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        present_mode: bevy::window::PresentMode::Immediate,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
+        .add_plugins(PerfUiPlugin)
         .add_systems(Startup, start)
         .add_systems(Update, update)
         .run();
 }
 
-fn start(
-    mut commands: Commands
-){
+fn start(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(PerfUiCompleteBundle::default());
 }
 
-fn update(
-    mut commands: Commands
-){
-    
-}
+fn update(mut commands: Commands) {}
